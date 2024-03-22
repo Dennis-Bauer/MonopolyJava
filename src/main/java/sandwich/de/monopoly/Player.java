e6f5921a365d58f7af7242f2dc8fb426fcad2aae
Dennis <bauer.denni@icloud.com>
2024-03-22 16:51:07 +0100

    Der Spieler verdient jetzt 200 Extra, wenn er über losläuft

--- src/main/java/sandwich/de/monopoly/Player.java
+++ src/main/java/sandwich/de/monopoly/Player.java
@@ -1,10 +1,17 @@
 package sandwich.de.monopoly;
 
+import sandwich.de.monopoly.DennisUtilitiesPackage.Java.ConsoleUtilities;
 import sandwich.de.monopoly.Enums.Figuren;
+import sandwich.de.monopoly.Exceptions.PlayerNotFoundExceptions;
+import sandwich.de.monopoly.GUI.Game.GameDisplayControllerOne;
+
+import static sandwich.de.monopoly.DennisUtilitiesPackage.Java.ConsoleUtilities.consoleOutPut;
+import static sandwich.de.monopoly.DennisUtilitiesPackage.Java.ConsoleUtilities.consoleOutPutLine;
 
 public class Player {
 
-    private int kontoStand;
+    private final int START_BANK_ACCOUNT = 2000;
+    private int bankAccount;
     private String name;
     private int fieldPostion;
     private final int orderNumber;
@@ -15,8 +22,8 @@ public class Player {
         this.figur = f;
         this.orderNumber = orderNumber;
 
-        kontoStand = Main.START_BANK_ACCOUNT;
-        fieldPostion = 20;
+        bankAccount = START_BANK_ACCOUNT;
+        fieldPostion = 0;
     }
 
     public String getName() {
@@ -25,10 +32,18 @@ public class Player {
 
     public int getKontoStand() {
         return kontoStand;
+    public int getBankAccount() {
+        return bankAccount;
+    }
+
     }
 
     public Figuren getFigur() {
         return figur;
+    }
+
+    public int getOrderNumber() {
+        return orderNumber;
     }
 
     public int getFieldPostion() {
@@ -37,13 +52,24 @@ public class Player {
 
     public void moveFieldPostion(int postions) {
         fieldPostion = fieldPostion + postions;
-        if (fieldPostion >= 40)
+        if (fieldPostion >= 40) {
             fieldPostion = fieldPostion - 40;
+
+            addBankAccount(200);
+
+            if (Main.CONSOLE_OUT_PUT) {
+                consoleOutPutLine(ConsoleUtilities.colors.WHITE, ConsoleUtilities.textStyle.REGULAR, Main.CONSOLE_OUT_PUT_LINEBREAK);
+                consoleOutPut(ConsoleUtilities.colors.GREEN, ConsoleUtilities.textStyle.REGULAR, "Der Spieler nummer " + orderNumber + " ist über los gegangen und hat");
+                consoleOutPutLine(ConsoleUtilities.colors.GREEN, ConsoleUtilities.textStyle.BOLD, " 200€ Plus gemacht!");
+                consoleOutPutLine(ConsoleUtilities.colors.WHITE, ConsoleUtilities.textStyle.REGULAR, Main.CONSOLE_OUT_PUT_LINEBREAK);
+            }
+        }
+
     }
 
     @Override
     public String toString() {
-        return "Name: " + name + ", Figure: " + figur.toString().toLowerCase() + ", FieldPosition: " + fieldPostion + ", Kontostand: " + kontoStand;
+        return "Name: " + name + ", Figure: " + figur.toString().toLowerCase() + ", FieldPosition: " + fieldPostion + ", Kontostand: " + bankAccount;
     }
 
 }
