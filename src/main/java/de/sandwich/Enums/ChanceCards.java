78f30518d1c166b7913bfba187453d0c0b9173a5
Sandwich898Mac <sandwich898.gaming@gmail.com>
2024-04-25 18:03:51 +0200

    Die Karten information wurden hinzugefügt und so gespeichert

--- src/main/java/de/sandwich/Enums/ChanceCards.java
+++ src/main/java/de/sandwich/Enums/ChanceCards.java
@@ -1,4 +1,32 @@
 package de.sandwich.Enums;
 
-public class ChanceCards {
+public enum ChanceCards {
+
+
+    //Extra
+    FREE_JAIL("Du kommst aus dem Gefängnis Frei"),
+    GO_TO_JAIL("Gehe ins Gefängnis, JETZT!"),
+    PAY_FINE("Zahle eine Strafe von -(200) oder nimm eine Gemeinschaftskarte."),
+    TO_SOUTH_STATION("Mache einen Ausflug nach dem Süd-Bahnhof. Wenn du über Los kommst, bekommst du +(2000)!"),
+    GO_3_BACK("Gehe 3 Felder zurück"),
+    GO_TO_START("Rücke bis auf Los vor."),
+    TO_FAST("Strafe für zu schnelles Fahren. (-300)"),
+    GO_TO_STREET_1("Rücke vor bis zu NAME_VON_FELT."),
+    GO_TO_STREET_2("Rücke vor bis zu NAME_VON_FELT."),
+    GO_TO_STREET_3("Rücke vor bis zu NAME_VON_FELT."),
+    GO_TO_STREET_4("Rücke vor bis zu NAME_VON_FELT."),
+    GO_TO_NEXT_STATION("Rücke vor bis zum nächsten Bahnhof. Der Eigentümer erhält das doppelte der normalen Miete. Hat noch kein Spieler einen Besitzanspruch auf diesen Bahnhof, so kann er ihn von der Bank kaufen."),
+
+    //Normal
+    YOU_GOT_CHOSED("Du wurdest zum Vorstand gewählt. Zahle jedem Spieler (-1000)!"),
+    HOUSES_RENOVATE("Du lässt alle deine Häuser renoviren. Zahle an die Bank (-500) je Haus und (-2000) je Hotel!"),
+    BANK_MONEY_1("Die Bank zahlt dir eine Dividende von (+1000)."),
+    BANK_MONEY_2("Mite und Anleihezinsen werden fällig. Die Bank zahlt dir (+3000)");
+
+    private final String MESSAGE;
+
+    ChanceCards(String m) {
+        this.MESSAGE = m;
+    }
+
 }
--- src/main/java/de/sandwich/Enums/CommunityCards.java
+++ src/main/java/de/sandwich/Enums/CommunityCards.java
@@ -1,4 +1,62 @@
 package de.sandwich.Enums;
 
 public enum CommunityCards {
+
+    //Extra
+    FREE_JAIL("Du kommst aus dem Gefängnis frei!", -1),
+    TO_START("Rücke vor bis auf Los.", -2),
+    GO_TO_JAIL("Gehe in das Gefängins!", -3),
+    BIRTHDAY("Es ist dein Geburtstag. Ziehe von jedem Spieler (+1000) ein.", -4),
+    HOUSES_MONEY("Du wirst zu Strassenausbesserungsarbeiteb herangezogen. Zahle für deine Häuse und Hotels (+800) je Haus und (+2300) je Hotel and die Bank", -5),
+
+    //Normal
+    PAY_HOSPITAL("Zahle an das Krankenhaus (-2000).", -2000),
+    STORAGE_GET_MONEY("Aus Lagerverkäufen erhälst du (+5000).", 5000),
+    SCHOOL_MONEY("Zahle Schulgeld. (-3000)", -3000),
+    SHARE_MONEY("Du erhältst auf Vorzugs-Aktien 7% Dividende. (+900)", 900),
+    PENSION_MONEY("Deine Jahresrente wird fällig. (+2000)", 2000),
+    BANK_MISTAK("Bank-Irrtum zu deinem Gunsten. (+4000)", 4000),
+    COMPOTION_WON("Du hast in einem Kreuzworträtsel-Wettbewerb gewonnen. (+2000)", 2000),
+    DOCTOR_COST("Artzt-Kosten. (-1000)", -1000),
+    TAX_REPAYMENT("Einkommensteuer-Rückzahlung. (+400)", 400),
+    HERITAGE("Du erbst. (+2000)", 2000),
+    BEAUTIFUL("Du hast den 2. Preis in einer Schönheitskonkurrenz gewonnen. (+200)", 200);
+
+
+    private final String MESSAGE;
+    private final int MONEY_TRANSFER;
+
+    CommunityCards(String m, int moneyTransfer) {
+        this.MESSAGE = m;
+
+        switch (moneyTransfer) {
+            case -1 -> {
+                //Komm aus dem Gefängins
+            }
+            case -2 -> {
+                //Geh zu los
+            }
+            case -3 -> {
+                //Ins gefängnis
+            }
+            case -4 -> {
+                //Jeder Spieler muss dem Activen Spieler 1000 bekommen
+            }
+            case -5 -> {
+                //Zahle je Haus 800 und je hotel 2300
+            }
+        }
+        this.MONEY_TRANSFER = moneyTransfer;
+    }
+
+    public int getMoneyTransfer() {
+        if (!(MONEY_TRANSFER <= -1 && MONEY_TRANSFER >= -5))
+            return MONEY_TRANSFER;
+        else throw new NullPointerException("The program tried to access money transfer on a card that don't have any.");
+    }
+
+    public void getAction() {
+        
+    }
+
 }
